57340ef4a91ccc2bbc52755fe661b174eb6f0b96
jeremy <jeremy.hoarau@supinfo.com>
2020-10-22 14:21:14 +0400

    Bouncy_Wall + Level_3 first part

---
+++ Assets/Scripts/BouncyWall.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BouncyWall : MonoBehaviour
+{
+    public float force = 950, yForce = 200;
+    
+    private bool inCooldown;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(inCooldown)
+            return;
+        Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+        Vector3 vel = rb.velocity;
+        if (rb == null)
+            return;
+        Vector3 currentDir = new Vector3(rb.velocity.x, 0, rb.velocity.z).normalized;
+        Vector3 dir = Vector3.Reflect(currentDir, transform.forward).normalized;
+        vel.y = 0;
+        rb.velocity = Vector3.zero;
+        rb.AddForce(dir * force + Vector3.up * yForce, ForceMode.Impulse);
+        //rb.AddForce(transform.forward * force + Vector3.up * yForce, ForceMode.Impulse);
+        StartCoroutine(Cooldoown());
+    }
+
+    IEnumerator Cooldoown()
+    {
+        inCooldown = true;
+        yield return new WaitForSeconds(0.1f);
+        inCooldown = false;
+    }
+}
